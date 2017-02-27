a5e708425e70b4c328962936fe6034c8b6d660e9
Ashish Lavania <a1lavania@gmail.com>
2017-02-28 00:01:04 +0900

    Made really small

--- cis194/assignments/03-sol/Golf.hs
+++ cis194/assignments/03-sol/Golf.hs
@@ -1,37 +1,25 @@
 module Golf where
 
-import Data.List
+r=length
 
-skips []= []
-skips q = [ (s x q) | x <- [1..(length q)]]
-
-t n [] = []
-t n (x:q) = x:(s n q)
+skips q = [ s x (drop (x-1) q) | x <- [1..r q]]
 
 s n [] = []
-s n l@(x:q) = t n (drop (n-1) l)
+s n l@(x:q) = x:(s n (drop n l))
 
-localMaxima (x:l@(y:z:q)) 
-  | (y>x && y>z) = y:p 
+localMaxima = k
+k (x:l@(y:z:q))
+  | (y>x && y>z) = y:p
   | True = p
-  where p = localMaxima l 
-localMaxima _ = []
+  where p = k l
+k _ = []
 
-histogram l = unlines ((h (map (c l) [0..9])) ++ "==========":[['0'..'9']])
+histogram l = unlines (h (map (c l) [0..9]) ++ "==========":[['0'..'9']])
 
-c (x:q) y 
-  |(x==y) = (1 + p)
-  | True = p
-  where p = c q y
-c _ _= 0
+c l y = r (filter (\x -> (x==y)) l)
 
-h l 
-  | (p == 0) = [] 
-  | True = reverse [w l x |x <- [1..p]]
-    where p = maximum l
+h l = reverse [map (f x) l |x <- [1..maximum l]]
 
-w (x:q) m 
-  | (x>=m) = '*':p 
-  | True = ' ':p
-  where p=w q m
-w _ _ = []
+f m x
+  | (m<=x) = '*'
+  | True = ' '
